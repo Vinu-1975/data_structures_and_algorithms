0561a3f46425aae68510b489338f315d1549bd4d
vinayakan <vinayakanvs2003@gmail.com>
2023-10-04 21:47:40 +0530

    updated dijkstra to provide path to destination

--- Algorithms/Greedy_Algo/Dijkstra/dijkstra_using_priorityQ.py
+++ Algorithms/Greedy_Algo/Dijkstra/dijkstra_using_priorityQ.py
@@ -1,7 +1,8 @@
 from heapq import heappush, heappop
-def dijkstra(graph,start):
+def dijkstra(graph,start,end):
 
     shortest_path = { vertex:float('inf') for vertex in graph}
+    predecessor = {vertex:None for vertex in graph}
     shortest_path[start] = 0
     pq = [(0,start)]
     while pq:
@@ -14,9 +15,16 @@ def dijkstra(graph,start):
             distance = current_weight + weight
             if distance < shortest_path[neighbor]:
                 shortest_path[neighbor] = distance
+                predecessor[neighbor] = current_vertex
                 heappush(pq,(distance,neighbor))
 
-    return shortest_path
+    path = []
+    while end:
+        path.append(end)
+        end = predecessor[end]
+    path.reverse()
+
+    return shortest_path,path
 
 graph = {
     'A': [('B', 1), ('C', 4)],
@@ -24,4 +32,4 @@ graph = {
     'C': [('A', 4), ('B', 2), ('D', 1)],
     'D': [('B', 5), ('C', 1)]
 }
-print(dijkstra(graph, 'A'))  # {'A': 0, 'B': 1, 'C': 3, 'D': 4}
+print(dijkstra(graph, 'A','D'))  # {'A': 0, 'B': 1, 'C': 3, 'D': 4}
